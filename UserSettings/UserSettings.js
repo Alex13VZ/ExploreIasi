8b82d1e99c83c97754ab7b7da0916c0d3d5e1bb5
Alex13VZ <alexandruzaharia2018@yahoo.com>
2022-10-23 22:29:12 +0300

    new update to change language

--- UserSettings/UserSettings.js
+++ UserSettings/UserSettings.js
@@ -18,6 +18,49 @@ document.querySelector(".en").addEventListener("click", () => {
   document.querySelector(".limba").classList.add("limba-visible");
 });
 
+document.querySelector(".X").addEventListener("click", () =>{
+  document.querySelector(".limba").classList.remove("limba-visible");
+}
+)
+
+document.querySelector(".limba1").addEventListener("click", () => {
+  document.querySelector(".check").classList.add("check-visible");
+});
+
+
+document.querySelector(".limba2").addEventListener("click", () => {
+  document.querySelector(".check1").classList.add("check-visible");
+  document.querySelector(".check").classList.remove("check-visible");
+  
+});
+
+
+document.querySelector(".limba3").addEventListener("click", () => {
+  document.querySelector(".check2").classList.add("check-visible");
+  document.querySelector(".check1").classList.remove("check-visible");
+  
+});
+
+
+document.querySelector(".limba4").addEventListener("click", () => {
+  document.querySelector(".check3").classList.add("check-visible");
+  document.querySelector(".check2").classList.remove("check-visible");
+  
+});
+
+
+document.querySelector(".limba5").addEventListener("click", () => {
+  document.querySelector(".check4").classList.add("check-visible");
+  document.querySelector(".check3").classList.remove("check-visible");
+  
+});
+
+
+
+
+
+
+
 function goBackToHome() {
   console.log("mi");
   window.location.href = "/home/home.html";
@@ -27,3 +70,5 @@ function goToSearch() {
   console.log("mi");
   window.location.href = "/search/search.html";
 }
+
+
