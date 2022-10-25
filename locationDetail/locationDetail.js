f569b6cbfb847b3945595ecb38db0ccae564ac9d
Alex13VZ <alexandruzaharia2018@yahoo.com>
2022-10-25 20:35:18 +0300

    clock page

--- locationDetail/locationDetail.js
+++ locationDetail/locationDetail.js
@@ -2,3 +2,11 @@ function GoBackToSearch() {
   console.log("i see");
   window.location.href = "/search/search.html";
 }
+
+document.querySelector(".p2").addEventListener("click", () => {
+  document.querySelector(".time").classList.add("time-visible");
+});
+
+document.querySelector(".BtnClose").addEventListener("click", () => {
+  document.querySelector(".time").classList.remove("time-visible");
+});
