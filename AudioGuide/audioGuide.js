6fa28008d317f5b5722b0d4f19f25877473e4a37
Alex13VZ <alexandruzaharia2018@yahoo.com>
2022-11-10 03:24:21 +0200

    sfarsit de proiect

---
+++ AudioGuide/audioGuide.js
@@ -0,0 +1,4 @@
+function goBackToMap() {
+  console.log("check");
+  window.location.href = "/TourDetailMap/tourDetailMap.html";
+}
--- TourDetailMap/tourDetailMap.js
+++ TourDetailMap/tourDetailMap.js
@@ -2,3 +2,8 @@ function goBackToTDTlList() {
   console.log("check");
   window.location.href = "/tourDetailList/tourDetailList.html";
 }
+
+function goToAudiPage() {
+  console.log("click");
+  window.location.href = "/AudioGuide/audioGuide.html";
+}
