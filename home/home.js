c1758dddfa12856733180e5469bc004ce7bf095a
Alex13VZ <alexandruzaharia2018@yahoo.com>
2022-11-15 23:29:06 +0200

    fixes

---
+++ home/home.js
@@ -0,0 +1,38 @@
+function goToUserSettings() {
+  console.log("mia");
+  window.location.href = "/UserSettings/UserSettings.html";
+}
+
+var i = 0; // Start Point
+var images = []; // Images Array
+var time = 3000; // Time Between Switch
+
+// Image List
+images[0] = "/home/media/pozaPalat.png";
+images[1] = "/home/media/insidePalace.jpg";
+images[2] = "/home/media/insidePalace1.jpg";
+images[3] = "/home/media/outsidePalace.jpg";
+
+// Change Image
+function changeImg() {
+  document.slide.src = images[i];
+
+  // Check If Index Is Under Max
+  if (i < images.length - 1) {
+    // Add 1 to Index
+    i++;
+  } else {
+    // Reset Back To O
+    i = 0;
+  }
+
+  // Run function every x seconds
+  setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload = changeImg;
+
+function GotOsearchPage() {
+  window.location.href = "/search/search.html";
+}
---
+++ search/search.js
@@ -0,0 +1,9 @@
+function goBcackToUs() {
+  console.log("mi");
+  window.location.href = "/home/home.html";
+}
+
+function goToLocDetail() {
+  console.log("push");
+  window.location.href = "/locationDetail/locationDetail.html";
+}
