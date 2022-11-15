d70c398f2f460ed983e3b70942de3be25fd0e545
Alex13VZ <alexandruzaharia2018@yahoo.com>
2022-11-16 00:36:34 +0200

    add audio to project

--- AudioGuide/audioGuide.js
+++ AudioGuide/audioGuide.js
@@ -1,4 +1,18 @@
+var playAudio = true;
+var audio = new Audio("/AudioGuide/media/jazz.mp3");
+
+function playMusic() {
+  if (playAudio) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+  playAudio = !playAudio;
+  console.log((audio.currentTime).toFixed(2))
+}
+
 function goBackToMap() {
   console.log("check");
   window.location.href = "/TourDetailMap/tourDetailMap.html";
 }
+
