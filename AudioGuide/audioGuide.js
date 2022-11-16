eaf3c42268515a06ce5b703774837d4a76f8a2c0
Alex13VZ <alexandruzaharia2018@yahoo.com>
2022-11-16 03:13:27 +0200

    add song with js

--- AudioGuide/audioGuide.js
+++ AudioGuide/audioGuide.js
@@ -8,11 +8,46 @@ function playMusic() {
     audio.pause();
   }
   playAudio = !playAudio;
-  console.log((audio.currentTime).toFixed(2))
 }
 
 function goBackToMap() {
-  console.log("check");
   window.location.href = "/TourDetailMap/tourDetailMap.html";
 }
 
+
+setInterval(() => {
+  var timer = Math.trunc(audio.currentTime);
+
+  const minutes = Math.floor(timer / 60);
+
+  const seconds = timer % 60;
+
+  function padTo2Digits(num) {
+    return num.toString().padStart(2, "0");
+  }
+
+  const currentTimer = `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
+  document.getElementById("time").innerHTML = currentTimer;
+
+  var remainTimer = Math.trunc(audio.duration) - timer;
+  const remainingMinutes = Math.floor(remainTimer / 60);
+  const remainingSeconds = remainTimer % 60;
+
+  const remainingTimer = `${padTo2Digits(remainingMinutes)}:${padTo2Digits(
+    remainingSeconds
+  )}`;
+  document.getElementById("time-left").innerHTML = remainingTimer;
+
+  //change width of progressbar
+  var elem = document.getElementById("progress-bar");
+  var songPos = document.getElementById("song-position");
+  var percentageOfSong = ((timer + 0.25) / Math.trunc(audio.duration)) * 100;
+  // var percentageOfSongPos = ((timer + 0.25) / Math.trunc(audio.duration)) * 70;
+  
+
+  elem.style.width = percentageOfSong + "%";
+  songPos.style.left = percentageOfSong - 2 + "vW";
+}, 1000);
+
+// la biluta ne jucam cu LEFT din CSS
+// la progressbar ne jucam cu width de pe class="w3-progressbar w3-round-xlarge"
