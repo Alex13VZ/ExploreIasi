bc6e4182340e2f9d435e7065f46fa6452495ac3e
Alex13VZ <alexandruzaharia2018@yahoo.com>
2023-01-09 23:29:23 +0200

    new function

--- search/search.js
+++ search/search.js
@@ -7,3 +7,36 @@ function goToLocDetail() {
   console.log("push");
   window.location.href = "/locationDetail/locationDetail.html";
 }
+
+const searchInput = document.getElementById('search-input');
+const recentSearches = document.getElementById('recent-searches');
+
+const searches = [
+  'Palatul Culturii',
+  'Universitatea Alexandru Ioan Cuza',
+  'Union Museum',
+  'Dosoftei House Museum',
+  'Copou Gardens',
+  'Botanical Gardens',
+  'Golia Monastery',
+  'Cetatuia Monastery'
+];
+
+searchInput.addEventListener('focus', () => {
+  recentSearches.innerHTML = ''; // Clear recent searches
+
+  searches.forEach(search => {
+    const div = document.createElement('div');
+    div.textContent = search;
+    div.addEventListener('click', () => {
+      searchInput.value = search;
+    });
+    recentSearches.appendChild(div);
+  });
+});
+
+document.addEventListener('click', event => {
+  if (event.target !== searchInput && !recentSearches.contains(event.target)) {
+    recentSearches.style.display = 'none';
+  }
+});
