12423dbca7017ab0c883f03c0efe56fb59af935f
Alex13VZ <alexandruzaharia2018@yahoo.com>
2023-01-16 23:58:49 +0200

    final ex 2

--- Api Calls/api.js
+++ Api Calls/api.js
@@ -1,38 +1,57 @@
-let allData;
+// let allData;
 
-// api url
-const api_url = "https://www.fishwatch.gov/api/species/red-snapper";
+// // api url
+// const api_url = "https://www.fishwatch.gov/api/species/red-snapper";
 
-function createElement(dataFromUrl) {
-	let titlu = document.createElement("h4");
-	titlu.innerHTML = dataFromUrl;
-   let main = document.getElementById("main");
-   main.appendChild(titlu);
-   let imagine = document.createElement("img");
-   document.getElementById("poze").appendChild(imagine);
-   imagine.className= "picture";
-//    imagine.src = dataFromUrl.Image_Gallery[0].src;
-   imagine.src = "https://origin-east-01-drupal-fishwatch.woc.noaa.gov/sites/default/files/snapperred_1.jpg";
+// function createElement(dataFromUrl) {
+// 	let titlu = document.createElement("h4");
+// 	titlu.innerHTML = dataFromUrl;
+//    let main = document.getElementById("main");
+//    main.appendChild(titlu);
+//    let imagine = document.createElement("img");
+//    document.getElementById("poze").appendChild(imagine);
+//    imagine.className= "picture";
+// //    imagine.src = dataFromUrl.Image_Gallery[0].src;
+//    imagine.src = "https://origin-east-01-drupal-fishwatch.woc.noaa.gov/sites/default/files/snapperred_1.jpg";
 
-	
-  }
+//   }
 
-// Defining async function
-async function getapi(url) {
-  // Storing response
-  const response = await fetch(url);
+// // Defining async function
+// async function getapi(url) {
+//   // Storing response
+//   const response = await fetch(url);
 
-  // Storing data in form of JSON
-  var data = await response.json();
-  allData = data[0];
-  console.log(allData);
-  if (response) {
-   createElement(allData.Calories);
-  }
-  //   show(data);
-}
-// Calling that async function
-getapi(api_url);
-
+//   // Storing data in form of JSON
+//   var data = await response.json();
+//   allData = data[0];
+//   console.log(allData);
+//   if (response) {
+//    createElement(allData.Calories);
+//   }
+//   //   show(data);
+// }
+// // Calling that async function
+// getapi(api_url);
 
 
+// exercitiu 2
+
+// let array = ["ala", "bala", "portocala"];
+// function RandomWords() {
+//   for (let index = 0; index < array.length; index++) {
+    
+//     const element = array;
+//     let a = document.createElement("p");
+//     a.innerHTML = element;
+//     document.getElementById("myAr").appendChild(a);
+//     array.push(array.shift())
+
+//   }
+// }
+
+
+//exercitiu 3
+
+
+//creeaza un input de timp numar 
+// creeaza un buton care la apasarea lui sa mi afiseze 10% din valoarea numarului introdus in input
