4beb082028aa883d4f7da108b59bf10a3d24ae3d
Alex13VZ <alexandruzaharia2018@yahoo.com>
2023-02-03 00:09:03 +0200

    create icons and added icons , create form component

--- First-Angular-Project/my-first-project/src/app/app.module.ts
+++ First-Angular-Project/my-first-project/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FormComponentComponent } from './form-component/form-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    FormComponentComponent
   ],
   imports: [
     BrowserModule,
--- First-Angular-Project/my-first-project/src/app/home-page/home-page.component.ts
+++ First-Angular-Project/my-first-project/src/app/home-page/home-page.component.ts
@@ -3,15 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  public titlu = "ce faci";
+  // public titlu = 'ce faci';
+  public displayIcon = true;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
+  public openForm(): void {
+    console.log(this.displayIcon);
   }
-
 }
